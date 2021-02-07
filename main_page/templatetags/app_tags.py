a34abafecbc310027159a4a29eca0e2e37773245
jonca3d <jonca3d@gmail.com>
2021-02-07 15:36:48 +0300

    Добавил к прокту статические файлы. Добавил свои стили для шаблона

--- main_page/templatetags/app_tags.py
+++ main_page/templatetags/app_tags.py
@@ -9,3 +9,10 @@ register = template.Library()
 def show_main_menu():
   return
 
+@register.inclusion_tag('include/notification_area.html')
+def show_notification_area():
+  return
+
+@register.inclusion_tag('include/other_information_area.html')
+def show_other_information_area():
+  return
